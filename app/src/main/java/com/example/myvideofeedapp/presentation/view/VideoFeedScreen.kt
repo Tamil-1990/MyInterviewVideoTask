c378615698ec6d47e41353953b38705e983a887e
admin <d.tamil90@gmail.com>
2026-05-27 22:52:24 +0530

    implement video streaming player play/pause and progress indicator

--- app/src/main/java/com/example/myvideofeedapp/presentation/view/VideoFeedScreen.kt
+++ app/src/main/java/com/example/myvideofeedapp/presentation/view/VideoFeedScreen.kt
@@ -1,18 +1,47 @@
 package com.example.myvideofeedapp.presentation.view
 
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.MediaItem
@@ -20,10 +49,10 @@ import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
-import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import com.example.myvideofeedapp.domain.model.VideoModel
 import com.example.myvideofeedapp.presentation.viewmodel.FeedViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun VideoFeedScreen(
@@ -39,7 +68,6 @@ fun VideoFeedScreen(
         }
     )
 
-
     LaunchedEffect(pagerState.currentPage) {
 
         if (
@@ -52,9 +80,8 @@ fun VideoFeedScreen(
     }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().systemBarsPadding()
     ) {
-
 
         if (
             state.isLoading &&
@@ -68,7 +95,6 @@ fun VideoFeedScreen(
             )
         }
 
-
         else {
 
             VerticalPager(
@@ -76,12 +102,10 @@ fun VideoFeedScreen(
                 modifier = Modifier.fillMaxSize()
             ) { page ->
 
-                val video =
-                    state.videos[page]
+                val video = state.videos[page]
 
                 VideoPlayerItem(
                     video = video,
-
                     isVisible =
                         pagerState.currentPage == page
                 )
@@ -98,6 +122,24 @@ fun VideoPlayerItem(
 
     val context = LocalContext.current
 
+    val coroutineScope =
+        rememberCoroutineScope()
+
+    var showCenterIcon by remember {
+        mutableStateOf(false)
+    }
+
+    var isPlaying by remember {
+        mutableStateOf(true)
+    }
+
+    var currentPosition by remember {
+        mutableLongStateOf(0L)
+    }
+
+    var duration by remember {
+        mutableLongStateOf(1L)
+    }
 
     val exoPlayer = remember(video.id) {
 
@@ -114,10 +156,11 @@ fun VideoPlayerItem(
                 repeatMode =
                     Player.REPEAT_MODE_ONE
 
+                playWhenReady = true
+
                 prepare()
             }
     }
-
 
     DisposableEffect(Unit) {
 
@@ -127,24 +170,73 @@ fun VideoPlayerItem(
         }
     }
 
-
     LaunchedEffect(isVisible) {
 
         if (isVisible) {
 
-            exoPlayer.playWhenReady = true
-
             exoPlayer.play()
+
+            isPlaying = true
 
         } else {
 
             exoPlayer.pause()
+
+            isPlaying = false
         }
     }
 
+    LaunchedEffect(exoPlayer) {
+
+        while (true) {
+
+            currentPosition =
+                exoPlayer.currentPosition
+
+            duration =
+                if (exoPlayer.duration > 0)
+                    exoPlayer.duration
+                else
+                    1L
+
+            delay(300)
+        }
+    }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .pointerInput(Unit) {
+
+                detectTapGestures(
+
+                    onTap = {
+
+                        if (exoPlayer.isPlaying) {
+
+                            exoPlayer.pause()
+
+                            isPlaying = false
+
+                        } else {
+
+                            exoPlayer.play()
+
+                            isPlaying = true
+                        }
+
+                        showCenterIcon = true
+
+                        coroutineScope.launch {
+
+                            delay(700)
+
+                            showCenterIcon = false
+                        }
+                    }
+                )
+            }
     ) {
 
         AndroidView(
@@ -153,29 +245,187 @@ fun VideoPlayerItem(
 
                 PlayerView(context).apply {
 
+                    player = exoPlayer
+
                     useController = false
 
                     resizeMode =
                         AspectRatioFrameLayout
                             .RESIZE_MODE_ZOOM
 
-                    player = exoPlayer
+                    setShowBuffering(
+                        PlayerView.SHOW_BUFFERING_ALWAYS
+                    )
                 }
             },
 
-            modifier = Modifier
-                .fillMaxSize()
-                .clickable {
+            modifier = Modifier.fillMaxSize()
+        )
 
-                    if (exoPlayer.isPlaying) {
+        AnimatedVisibility(
+            visible = showCenterIcon,
+            enter = fadeIn(),
+            exit = fadeOut(),
+            modifier = Modifier.align(
+                Alignment.Center
+            )
+        ) {
 
-                        exoPlayer.pause()
+            Box(
+                modifier = Modifier
+                    .size(90.dp)
+                    .clip(CircleShape)
+                    .background(
+                        Color.Black.copy(alpha = 0.5f)
+                    ),
 
-                    } else {
+                contentAlignment =
+                    Alignment.Center
+            ) {
 
-                        exoPlayer.play()
-                    }
+                if (isPlaying) {
+
+                    PlayIcon()
+
+                } else {
+
+                    PauseIcon()
                 }
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(
+                    start = 16.dp,
+                    bottom = 40.dp,
+                    end = 90.dp
+                )
+        ) {
+
+            /*Text(
+                text = "@android_dev",
+
+                color = Color.White,
+
+                fontSize = 18.sp,
+
+                fontWeight = FontWeight.Bold
+            )*/
+
+            Text(
+                text = "Vid: ${video.id}",
+
+                color = Color.White,
+
+                fontSize = 15.sp,
+
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        ) {
+
+            LinearProgressIndicator(
+
+                progress = {
+                    currentPosition.toFloat() /
+                            duration.toFloat()
+                },
+
+                color = Color.Red,
+
+                trackColor = Color.Gray.copy(alpha = 0.4f),
+
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Text(
+
+                text =
+                    "${((currentPosition * 100) / duration)}%",
+
+                color = Color.White,
+
+                fontSize = 12.sp,
+
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(
+                        end = 8.dp,
+                        top = 2.dp,
+                        bottom = 4.dp
+                    )
+            )
+        }
+    }
+}
+
+@Composable
+fun PlayIcon() {
+
+    Canvas(
+        modifier = Modifier.size(40.dp)
+    ) {
+
+        val path = Path().apply {
+
+            moveTo(size.width * 0.2f, size.height * 0.1f)
+
+            lineTo(size.width * 0.8f, size.height * 0.5f)
+
+            lineTo(size.width * 0.2f, size.height * 0.9f)
+
+            close()
+        }
+
+        drawPath(
+            path = path,
+            color = Color.White,
+            style = Fill
+        )
+    }
+}
+
+@Composable
+fun PauseIcon() {
+
+    Canvas(
+        modifier = Modifier.size(40.dp)
+    ) {
+
+        drawRect(
+            color = Color.White,
+
+            topLeft = Offset(
+                size.width * 0.2f,
+                size.height * 0.1f
+            ),
+
+            size = Size(
+                size.width * 0.2f,
+                size.height * 0.8f
+            )
+        )
+
+        drawRect(
+            color = Color.White,
+
+            topLeft = Offset(
+                size.width * 0.6f,
+                size.height * 0.1f
+            ),
+
+            size = Size(
+                size.width * 0.2f,
+                size.height * 0.8f
+            )
         )
     }
 }
